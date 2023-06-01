1bb1b3c9faf21159068e2753af603c889271b098
Killerpac <nilavasarkar@gmail.com>
2023-06-01 17:39:01 +0530

    nuke notify in game mode

--- src/com/realmeparts/services/GameModeTileService.java
+++ src/com/realmeparts/services/GameModeTileService.java
@@ -68,29 +68,9 @@ public class GameModeTileService extends TileService {
         mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         enabled = GameModeSwitch.isCurrentlyEnabled(this);
-        if (!enabled) {
-            AppNotification.Send(this, GameModeSwitch.GameMode_Notification_Channel_ID, this.getString(R.string.game_mode_title), this.getString(R.string.game_mode_notif_content));
-        } else AppNotification.Cancel(this, GameModeSwitch.GameMode_Notification_Channel_ID);
         Utils.writeValue(GameModeSwitch.getFile(), enabled ? "0" : "1");
         Utils.writeValue(DeviceSettings.TP_LIMIT_ENABLE, enabled ? "1" : "0");
         Utils.writeValue(DeviceSettings.TP_DIRECTION, enabled ? "0" : "1");
         SystemProperties.set("perf_profile", enabled ? "0" : "1");
-        if (sharedPrefs.getBoolean("dnd", false)) GameModeTileDND();
-        sharedPrefs.edit().putBoolean(DeviceSettings.KEY_GAME_SWITCH, !enabled).commit();
-        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
-        getQsTile().updateTile();
-    }
-
-    private void GameModeTileDND() {
-        switch ((GameModeSwitch.isCurrentlyEnabled(this)) ? 1 : 0) {
-            case 1:
-                mNotificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_PRIORITY);
-                mNotificationManager.setNotificationPolicy(
-                        new NotificationManager.Policy(NotificationManager.Policy.PRIORITY_CATEGORY_MEDIA, 0, 0));
-                break;
-            case 0:
-                mNotificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL);
-                break;
-        }
     }
 }
